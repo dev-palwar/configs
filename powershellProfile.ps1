300afcda028047e0af0cced6b9bdb065ccd74bdd
Dev Palwar <devpalwar06@gmail.com>
2024-01-16 17:32:56 +0530

    some fixes

--- powershellProfile.ps1
+++ powershellProfile.ps1
@@ -1,13 +1,19 @@
 Clear-Host
 
+# For the icons 
 Import-Module -Name Terminal-Icons
+
+# Activates intellisense 
 
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Windows
 
+# Configuration to posh theme 
+
 oh-my-posh init pwsh --config 'D:\Me\posh-configs\themes\shell.json' | Invoke-Expression
 
+# Some functions 
 function Open {
     param (
         [string]$site
